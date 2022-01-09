09deb3b0c9004f3a64a7e4e9545ae80cc02f27e1
chenr <c.henrique.cunha08@gmail.com>
2022-01-08 21:01:44 -0300

    adicionando ao github

---
+++ src/main/java/br/com/livraria/ingary/IngaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.livraria.ingary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/livraria/ingary/IngaryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.livraria.ingary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
